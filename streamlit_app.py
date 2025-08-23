d8e274d533e423d919ce54e71593f39ce440a484
Amarjit Singh Dhillon <amarjit314@gmail.com>
2025-08-23 23:17:20 +0000

    xx

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,17 @@ import requests
 import pandas as pd
 
 # Write directly to the app
-st.title(f"Amar's Smoothie App :cup_with_straw:")
-st.write(
-  """
-  Choose the fruits you want in your custom Smoothie!
-  """
-)
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
-name_on_order = st. text_input( 'Name on Smoothie: ')
-st.write('The name on your Smoothie will be:', name_on_order)
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on smoothie will be:", name_on_order)
 
-# Establish connection to Snowflake (assuming st.connection is correctly defined)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 # Convert the Snowpark Dataframe to Pandas Dataframe to use the LOC function
 pd_df = my_dataframe.to_pandas()
@@ -57,4 +53,5 @@ if ingredients_list:
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")  
+        
+        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
