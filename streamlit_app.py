451bfcdff8b92893aefb107f2760e7e956d4adc3
Amarjit Singh Dhillon <amarjit314@gmail.com>
2025-08-23 18:26:16 +0000

    xx

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f"Amar's Smoothie App :cup_with_straw:")
@@ -19,6 +21,11 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
+# Convert the Snowpark Dataframe to Pandas Dataframe to use the LOC function
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
     , my_dataframe
@@ -31,6 +38,13 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+        st.subheader(fruit_chosen + " Nutrition Information")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #  Write the list to app
     # st.write(ingredients_string)
