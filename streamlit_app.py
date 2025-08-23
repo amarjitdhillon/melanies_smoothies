5bdce84a457213d853dc1bb1e86e7d311d1943be
Amarjit Singh Dhillon <amarjit314@gmail.com>
2025-08-23 18:30:31 +0000

    xx

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,12 +39,16 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        # New section to display smoothiefroot nutrition information
+        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        # st. text (smoothiefroot_response.json())
+
+        # search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        st.subheader(fruit_chosen + " Nutrition Information")
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        # st.subheader(fruit_chosen + " Nutrition Information")
+        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        # fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #  Write the list to app
     # st.write(ingredients_string)
